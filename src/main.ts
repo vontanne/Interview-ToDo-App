bbb6a9259a1ab69fa96711dd3fbb0a573df994e3
Johannes <vontanne@gmail.com>
2024-12-01 20:07:55 +0400

    feat: add Swagger documentation for API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TODO App API')
+    .setDescription('API documentation for the TODO application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000, () => {
-    console.log('Server started listening on port 3000');
+    console.log('Server is running on http://localhost:3000');
   });
 }
 bootstrap();
