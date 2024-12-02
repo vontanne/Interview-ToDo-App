fed3ba07f204d9abc180fb43f5a5c90508f0a869
Johannes <vontanne@gmail.com>
2024-12-02 23:09:44 +0400

    feat: add TodoDto for validating and handling todo data

---
+++ src/modules/todo/dto/todo.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsEnum,
+  IsInt,
+  IsString,
+  Min,
+  Max,
+} from 'class-validator';
+import { TodoStatusEnum } from '../enum/todo-status.enum';
+
+export class TodoDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsEnum(TodoStatusEnum)
+  status?: TodoStatusEnum;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(3)
+  priority?: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  userId: number;
+}
