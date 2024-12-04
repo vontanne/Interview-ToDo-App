b3a1f3ce8f28067cf9762782ef1dc0805fb66369
Johannes <vontanne@gmail.com>
2024-12-04 18:55:06 +0400

    refactor: remove unused UserController

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
