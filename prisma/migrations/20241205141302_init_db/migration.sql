9cfc250fa68c5052c4d12a6c161dd53a3f5158f3
Johannes <vontanne@gmail.com>
2024-12-05 18:22:25 +0400

    feat: add title length validation to TodoDto

---
+++ prisma/migrations/20241205141302_init_db/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fullName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `refreshToken` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `status` ENUM('PENDING', 'COMPLETED', 'IN_PROGRESS') NOT NULL DEFAULT 'PENDING',
+    `priority` INTEGER NOT NULL DEFAULT 1,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Todo` ADD CONSTRAINT `Todo_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- src/modules/todo/dto/todo.dto.ts
+++ src/modules/todo/dto/todo.dto.ts
@@ -6,12 +6,14 @@ import {
   IsString,
   Min,
   Max,
+  Length,
 } from 'class-validator';
 import { TodoStatusEnum } from '../enum/todo-status.enum';
 
 export class TodoDto {
   @IsNotEmpty()
   @IsString()
+  @Length(1, 128)
   title: string;
 
   @IsOptional()
